c92279516cd20c5f3613eceb45ef729683ec8ddf
Ben Loesch <benloesch@gmail.com>
2018-06-06 22:24:33 -0700

    Second Commit: Attempting to add project to repo

---
+++ Assets/DiceRoll.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceRoll : MonoBehaviour {
+
+	public bool hasRolled;
+
+	// Use this for initialization
+	void Start () {
+		hasRolled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public int[] diceVals = new int[4];
+	public int diceTotal;
+
+	public void RollTheDice(){
+		diceTotal = 0;
+
+		//Quill's way...
+		for (int i = 0; i < diceVals.Length; i++) {
+			diceVals [i] = Random.Range (0, 2);
+			diceTotal += diceVals [i];
+		}
+		Debug.Log ("Rolled: " + diceVals + " (" + diceTotal + ")");
+		hasRolled = true;
+		//My way...
+	}
+}
---
+++ Assets/PlayerStone.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStone : MonoBehaviour {
+
+    DiceRoll theDiceRoller;
+    
+	// Use this for initialization
+	void Start () {
+        theDiceRoller = GameObject.FindObjectOfType<DiceRoll>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseUp()
+    {
+        int movableSpaces = theDiceRoller.diceTotal;
+        //TODO: Is mouse over UI element? If so, ignore this click.
+
+        //TODO: Is our turn?
+
+        //TODO: Has dice been rolled?
+        if (theDiceRoller.hasRolled == false)
+        {
+            Debug.Log("Please Roll a #.");
+            return;
+        }
+
+        if (theDiceRoller.hasRolled)
+        {
+            //Move this stone
+            Debug.Log("Meow!");
+        }
+
+    }
+}
