8564463fee6d803a80ef81d8752b0072362337a4
Ben Loesch <benloesch@gmail.com>
2018-06-06 22:33:57 -0700

    Technically this is 2nd commit. This is pushing a test script

---
+++ Assets/TestScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Testing 1...2...3");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
